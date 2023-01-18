07d02519e7e6a68c550bcc3094b82ec14f52b829
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2023-01-18 18:37:31 +0200

    adding first screen in application

--- app/src/main/java/com/example/fruity/WelcomeScreen.kt
+++ app/src/main/java/com/example/fruity/WelcomeScreen.kt
@@ -1,2 +1,21 @@
 package com.example.fruity
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fruity.ui.theme.FruityTheme
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier
+){
+
+}
+
+@Preview (showBackground = true, backgroundColor = 0XFFFFFFFF)
+@Composable
+fun WelcomeScreenPreview(){
+    FruityTheme {
+        WelcomeScreen()
+    }
+}
