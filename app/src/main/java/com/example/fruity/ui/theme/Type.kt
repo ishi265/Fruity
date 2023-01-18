a007d5a07679bcdefcaadc46b68cf945cb437bc7
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2023-01-18 18:14:00 +0200

    first commit

---
+++ app/src/main/java/com/example/fruity/ui/theme/Type.kt
@@ -0,0 +1,18 @@
+package com.example.fruity.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.fruity.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
