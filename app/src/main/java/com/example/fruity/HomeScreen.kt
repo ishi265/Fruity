8b0b47c9dd09eb2b92cf160c2e254f32781d294c
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2023-01-24 08:40:48 +0200

    my commit today

--- app/src/main/java/com/example/fruity/HomeScreen.kt
+++ app/src/main/java/com/example/fruity/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.fruity
 
+import android.graphics.Paint.Align
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -16,6 +22,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow.Companion.Clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -121,21 +128,53 @@ fun SearchBarPreview(){
 }
 
 @Composable
-fun CategoryItem(modifier: Modifier = Modifier){
-
+fun CategoryItem(
+    @DrawableRes image: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = modifier
+                .clip(shape = RoundedCornerShape(35.dp))
+                .background(AppStrongGrey)
+                .size(100.dp)
+                .padding(15.dp),
+            painter = painterResource(id = image),
+            contentDescription = null
+        )
+        Text(
+            stringResource(id = text),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            modifier = modifier
+                .padding(top = 15.dp)
+        )
+    }
 }
 
 @Preview (showBackground = true)
 @Composable
 fun CategoryItemPreview(){
     FruityTheme {
-        CategoryItem()
+        CategoryItem(
+            R.drawable.nut,
+            R.string.cat_nuts
+        )
     }
 }
 
 @Composable
 fun CategoryLazyItems(modifier: Modifier = Modifier){
-
+    LazyRow() {
+        items(CategoryItems) { item ->
+            CategoryItem(image = item.drawable, text = item.string)
+        }
+    }
 }
 
 @Preview (showBackground = true)
@@ -158,3 +197,20 @@ fun FruitItemLazyGrid(){
         FruitItem()
     }
 }
+
+
+
+//my data
+
+private val CategoryItems = listOf(
+    R.drawable.nut to R.string.cat_nuts,
+    R.drawable.apples to R.string.cat_fruits,
+    R.drawable.wheat to R.string.cat_vegetable,
+    R.drawable.chilli to R.string.cat_chilli,
+    R.drawable.wheat to R.string.cat_gluten
+).map {LazyCategory(it.first, it.second)}
+
+private data class LazyCategory(
+    @DrawableRes val drawable: Int,
+    @StringRes val string: Int
+)
